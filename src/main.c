3a01b28891f8d6822f1f69e275758e52c05d7b17
Matteo Golin <matteo.golin@gmail.com>
2023-11-02 12:12:30 -0400

    Added better option parsing and a functioning endless mode.

--- src/main.c
+++ src/main.c
@@ -4,20 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static bool endless = false; // Flag for endless mode
+static char *filename = NULL;
 #define BUFFER_SIZE 100
 
 int main(int argc, char **argv) {
 
-    int c;                // Holder for choice
-    bool endless = false; // Flag for endless mode
+    int c; // Holder for choice
     opterr = 0;
 
     /* Get command line options. */
-    while ((c = getopt(argc, argv, "e")) != -1)
+    while ((c = getopt(argc, argv, ":e:")) != -1)
         switch (c) {
         case 'e':
             endless = true;
+            filename = optarg;
             break;
+        case ':':
+            fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            exit(EXIT_FAILURE);
         case '?':
             fprintf(stderr, "Unknown option `-%c'.\n", optopt);
             exit(EXIT_FAILURE);
@@ -26,32 +31,29 @@ int main(int argc, char **argv) {
             exit(EXIT_FAILURE);
         }
 
-    /* Check to see if we also received the file argument after all the flags. */
-    if (optind >= argc) {
-        fputs("You must provide a file argument.", stderr);
-        exit(EXIT_FAILURE);
-    }
-    char *filename = argv[optind];
+    // Only read from a file if in endless mode
+    if (endless) {
 
-    /* Open file for reading. */
-    FILE *f = fopen(filename, "r");
-    if (f == NULL) {
-        fprintf(stderr, "File '%s' cannot be opened.\n", filename);
-        exit(EXIT_FAILURE);
-    }
+        /* Open file for reading. */
+        FILE *f = fopen(filename, "r");
+        if (f == NULL) {
+            fprintf(stderr, "File '%s' cannot be opened.\n", filename);
+            exit(EXIT_FAILURE);
+        }
 
-    /* Loop over file to get data. */
-    uint8_t buffer[BUFFER_SIZE] = {0};
-    for (;;) {
-        size_t items_read = fread(&buffer, sizeof(uint8_t), BUFFER_SIZE, f);
-        fwrite(&buffer, sizeof(uint8_t), items_read, stdout);
+        /* Loop over file to get data. */
+        uint8_t buffer[BUFFER_SIZE] = {0};
+        for (;;) {
+            size_t items_read = fread(&buffer, sizeof(uint8_t), BUFFER_SIZE, f);
+            fwrite(&buffer, sizeof(uint8_t), items_read, stdout);
 
-        // In endless mode, go back to the file start when we reach the end of the file
-        if (feof(f)) {
-            if (endless) {
-                rewind(f);
-            } else {
-                return 0; // Otherwise complete
+            // In endless mode, go back to the file start when we reach the end of the file
+            if (feof(f)) {
+                if (endless) {
+                    rewind(f);
+                } else {
+                    return 0; // Otherwise complete
+                }
             }
         }
     }
