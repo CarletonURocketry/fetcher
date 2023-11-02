035659dccc4779ec7b2db2ec1b6617355b18a205
linguini1 <linguini1@users.noreply.github.com>
2023-11-02 13:33:49 +0000

    deploy: 394637bf8fd82c86b1d812386d3106b85c57baab

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "m",
+  0: "bm",
   1: "m",
-  2: "m"
+  2: "m",
+  3: "b"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "files",
-  2: "functions"
+  2: "functions",
+  3: "defines"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Files",
-  2: "Functions"
+  2: "Functions",
+  3: "Macros"
 };
 
