b5a23d0b54e98d9bdbf7c15c5959b6b482172e01
AngusJull <angusjull1@gmail.com>
2024-03-27 23:27:43 -0400

    Add functions for reading, and proof i2c works
    * Much to my dissapointment, the problem is me, not i2c.
    * Added a function for only printing the next ublox message that can
      be found, within n seconds.
    * Additionally added a function that will dump the contents of
      the data buffer, reading a byte at a time so we can be sure the i2c
      interface is open.
    * Totally messed up the read and init function, left a bunch of garbage
      in there. Will need to be amended or squashed with a fix later

--- src/sensors/m10spg/m10spg.c
+++ src/sensors/m10spg/m10spg.c
@@ -9,8 +9,10 @@
 #include "../sensor_api.h"
 #include <errno.h>
 #include <hw/i2c.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 /** Macro to early return an error. */
@@ -95,35 +97,79 @@ static errno_t m10spg_write(Sensor *sensor, void *buf, size_t nbytes) {
 /**
  * Reads the specified data from the M10SPG.
  * @param sensor A reference to an M10SPG sensor.
- * @param tag The tag of the data type that should be read.
  * @param buf A pointer to the memory location to store the data.
- * @param nbytes The number of bytes that were written into the byte array buffer.
+ * @param nbytes The number of bytes to read into the buffer
  * @return errno_t The error status of the call. EOK if successful.
  */
-static errno_t m10spg_read(Sensor *sensor, const SensorTag tag, void *buf, size_t *nbytes) {
+static errno_t m10spg_read(Sensor *sensor, void *buf, size_t nbytes) {
+    i2c_recv_t header = {.stop = 1, .slave = sensor->loc.addr, .len = nbytes};
 
-    // Make sure in read mode
-    i2c_addr_t read_addr = sensor->loc.addr;
+    uint8_t read_cmd[sizeof(header) + nbytes];
+    memcpy(read_cmd, &header, sizeof(header));
 
-    for (int j = 0; j < 500; j++) {
-        i2c_sendrecv_t header = {.stop = 1, .slave = sensor->loc.addr, .recv_len = 3, .send_len = 0};
+    errno_t err = devctl(sensor->loc.bus, DCMD_I2C_RECV, read_cmd, sizeof(read_cmd), NULL);
+    if (err != EOK) return err;
 
-        uint8_t read_cmd[sizeof(header) + 3];
-        memcpy(read_cmd, &header, sizeof(header));
-
-        errno_t err = devctl(sensor->loc.bus, DCMD_I2C_SENDRECV, read_cmd, sizeof(read_cmd), NULL);
-        if (err != EOK) return err;
-
-        for (uint8_t i = 0; i < 3; i++) {
-            // printf("%x", read_cmd[sizeof(header) + i]);
-            putchar(read_cmd[sizeof(header) + i]);
-        }
-    }
-    printf("\n");
-
+    // Copy out what we recieved
+    memcpy(buf, read_cmd + sizeof(header), nbytes);
     return EOK;
 }
 
+static errno_t dump_buffer(Sensor *sensor, size_t bytes) {
+    for (size_t i = 0; i < bytes; i++) {
+        uint8_t byte;
+        m10spg_read(sensor, &byte, 1);
+        printf("%x ", byte);
+    }
+    putchar('\n');
+}
+
+#define BUF_SIZE 500
+static errno_t get_next_ublox(Sensor *sensor, uint8_t timeout) {
+    static uint8_t read_buffer[BUF_SIZE];
+    // Clear the last time we did this
+    memset(read_buffer, 0, sizeof(read_buffer));
+    struct timespec start, stop;
+    clock_gettime(CLOCK_REALTIME, &start);
+    clock_gettime(CLOCK_REALTIME, &stop);
+    while ((stop.tv_sec - start.tv_sec) < timeout) {
+        errno_t err = m10spg_read(sensor, read_buffer, 1);
+        if (err != EOK) return err;
+
+        if (read_buffer[0] == H1) {
+            err = m10spg_read(sensor, read_buffer + 1, 1);
+            if (err != EOK) return err;
+            if (read_buffer[1] == H2) {
+                // Found something. Get message length
+                err = m10spg_read(sensor, read_buffer + 2, 4);
+                if (err != EOK) return err;
+
+                uint16_t length = (read_buffer[4] + read_buffer[5] * 256);
+                printf("Found a message length : %d", length);
+                err = m10spg_read(sensor, read_buffer + 6, length + 2);
+                if (err != EOK) return err;
+
+                printf("Message found \n(hex): ");
+                for (uint16_t j = 0; j < length + 6; j++) {
+                    printf("%x ", read_buffer[j]);
+                }
+                putchar('\n');
+
+                printf("(ascii): ");
+                for (uint16_t j = 0; j < length + 6; j++) {
+                    putchar(read_buffer[j]);
+                }
+                putchar('\n');
+                return EOK;
+            } else {
+                return EBADMSG;
+            }
+        }
+        clock_gettime(CLOCK_REALTIME, &stop);
+        usleep(10000);
+    }
+    return ETIMEDOUT;
+}
 /**
  * Returns the number of bytes ready to be read from the sensor
  *
@@ -153,6 +199,15 @@ static errno_t m10spg_available_bytes(Sensor *sensor, uint16_t *result) {
     return EOK;
 }
 
+static void calculate_ublox_checksum(uint8_t *bytes, uint8_t nbytes, uint8_t *ck_a, uint8_t *ck_b) {
+    *ck_a = 0;
+    *ck_b = 0;
+    for (uint8_t *byte = bytes + 2; byte < bytes + nbytes - 3; byte++) {
+        *ck_a = *ck_a + *byte;
+        *ck_b = *ck_b + *ck_a;
+    }
+}
+
 /**
  * Prepares the M10SPG for reading.
  * @param sensor A reference to an M10SPG sensor.
@@ -172,13 +227,32 @@ void m10spg_init(Sensor *sensor, const int bus, const uint8_t addr, const Sensor
     sensor->loc = (SensorLocation){.bus = bus, .addr = {.addr = (addr & 0x42), .fmt = I2C_ADDRFMT_7BIT}};
     sensor->tag_list = (SensorTagList){.tags = TAGS, .len = sizeof(TAGS) / sizeof(SensorTag)};
     sensor->context.size = 0;
-    sensor->read = &m10spg_read;
     sensor->open = &m10spg_open;
 
     // Taken from someone else's code example
     uint8_t ubx_mon_ver[] = {H1, H2, 0x0A, 0x04, 0x00, 0x00, 0x0E, 0x34};
     uint8_t ubx_mon_hw3[] = {H1, H2, 0x0A, 0x37, 0x00, 0x00, 0x41, 0xcd};
+    uint8_t ubx_mon_gnss[] = {H1, H2, 0x0a, 0x28, 0x00, 0x00, 0x32, 0xa0};
+    uint8_t ubx_mon_comms[] = {H1, H2, 0x0a, 0x36, 0x00, 0x00, 0x40, 0xca};
+    uint32_t key = 0x10510003;
+    uint8_t *key_bytes = (uint8_t *)&key;
+    uint8_t ubx_i2c_enabled[] = {H1,   H2,   0x06, 0x8B, 0x08, 0x00, 0x00, 0x00,
+                                 0x00, 0x00, 0x03, 0x00, 0x51, 0x10, 0xfd, 0x4f};
 
-    m10spg_write(sensor, ubx_mon_ver, sizeof(ubx_mon_ver));
-    m10spg_read(sensor, TAG_TIME, NULL, 0);
+    m10spg_write(sensor, ubx_i2c_enabled, sizeof(ubx_i2c_enabled));
+    sleep(1);
+    printf("\n Return: %d\n", get_next_ublox(sensor, 10));
+    // dump_buffer(sensor, 1000);
+    //  printf("\nFinal code: %d\n", get_next_ublox(sensor, 10));
+    /*
+        m10spg_write(sensor, ubx_mon_hw3, sizeof(ubx_mon_hw3));
+        printf("\nFinal code: %d\n", get_next_ublox(sensor, 10));
+
+        m10spg_write(sensor, ubx_mon_comms, sizeof(ubx_mon_comms));
+        printf("\nFinal code: %d\n", get_next_ublox(sensor, 10));
+
+        while (1) {
+            sleep(1);
+            printf("\nFinal code: %d\n", get_next_ublox(sensor, 10));
+        }*/
 }
