13667432f529b1d63cbebb1045739bd354f95b0a
Matteo Golin <matteo.golin@gmail.com>
2024-04-13 16:58:06 -0400

    Add control registers and configure for high performance mode.

--- src/sensors/lsm6dso32/lsm6dso32.c
+++ src/sensors/lsm6dso32/lsm6dso32.c
@@ -23,6 +23,17 @@ typedef enum {
     OUTX_L_A = 0x29,   /**< X-axis linear acceleration (16 bits) */
     TIMESTAMP0 = 0x40, /**< First timestamp register (32 bits total) */
     STATUS_REG = 0x1E, /**< The status register of whether data is available. */
+    CTRL1_XL = 0x10,   /**< Accelerometer control register 1 */
+    CTRL2_G = 0x11,    /**< Gyroscope control register 2 */
+    CTRL3_C = 0x12,    /**< Control register 3 */
+    CTRL4_C = 0x13,    /**< Control register 4 */
+    CTRL5_C = 0x14,    /**< Control register 5 */
+    CTRL6_C = 0x15,    /**< Control register 6 */
+    CTRL7_G = 0x16,    /**< Control register 7 */
+    CTRL8_XL = 0x17,   /**< Control register 8 */
+    CTRL9_XL = 0x18,   /**< Control register 9 */
+    CTRL10_C = 0x19,   /**< Control register 10 */
+    FIFO_CTRL4 = 0x0A, /**< The fourth FIFO control register (for setting continuous mode) */
 } IMUReg;
 
 /** Macro to early return an error. */
@@ -32,26 +43,36 @@ typedef enum {
 /** A list of data types that can be read by the LSM6DSO32. */
 static const SensorTag TAGS[] = {};
 
-static errno_t lsm6dso32_write(Sensor const *sensor, void *buf, size_t nbytes) {
+/**
+ * Write data to a register of the LSM6DS032.
+ * @param sensor A pointer to a LSM6DS032 sensor instance.
+ * @param reg The address of the register to write to.
+ * @param data The byte of data to write to the register.
+ */
+static errno_t lsm6dso32_write_byte(Sensor const *sensor, const uint8_t reg, const uint8_t data) {
 
     // Construct header
-    static i2c_send_t header = {.slave = {0}, .stop = 1, .len = 0};
-    header.len = nbytes;
+    static i2c_send_t header = {.slave = {0}, .stop = 1, .len = 2};
     header.slave = sensor->loc.addr;
 
     // Create command
-    uint8_t cmd[nbytes + sizeof(header)];
+    static uint8_t cmd[sizeof(header) + 2];
     memcpy(cmd, &header, sizeof(header));
-    memcpy(&cmd[sizeof(header)], buf, nbytes);
+    cmd[sizeof(header)] = reg;
+    cmd[sizeof(header) + 1] = data;
 
     // Send command
-    return devctl(sensor->loc.bus, DCMD_I2C_SEND, cmd, nbytes + sizeof(header), NULL);
+    return devctl(sensor->loc.bus, DCMD_I2C_SEND, cmd, sizeof(cmd), NULL);
 }
 
 /**
  * Read `nbytes` from starting at register address `reg`.
  * WARNING: `buf` must contain enough space for `nbytes` bytes AND `sizeof(i2c_recv_t)`. Do not include the extra byte
  * count in nbytes.
+ * @param sensor A reference to an LSM6DS032 sensor instance.
+ * @param reg The register address to read from.
+ * @param buf The buffer to read data into.
+ * @param nbytes The number of bytes to read.
  */
 static errno_t lsm6dso32_read_bytes(Sensor *sensor, uint8_t reg, void *buf, size_t nbytes) {
 
@@ -96,8 +117,18 @@ static errno_t lsm6ds032_read(Sensor *sensor, const SensorTag tag, void *buf, si
  */
 static errno_t lsm6ds032_open(Sensor *sensor) {
 
-    uint8_t config[] = {0x10, 0x8};
-    errno_t err = lsm6dso32_write(sensor, &config, sizeof(config));
+    // TODO: We will want to operate in continuous mode for our use case (polling)
+
+    // Set the operating mode of the accelerometer to ODR of 6.66kHz (high perf)
+    // TODO: set based on configured sensor performance
+    // Full scale rang of +-32g (necessary for rocket)
+    errno_t err = lsm6dso32_write_byte(sensor, CTRL1_XL, 0xA4);
+    return_err(err);
+
+    // Set the operating mode of the gyroscope to ODR of 6.66kHz (high perf)
+    // TODO: set based on configured sensor performance
+    // TODO: what full-scale selection?
+    err = lsm6dso32_write_byte(sensor, CTRL2_G, 0xA0);
     return_err(err);
 
     uint8_t result[sizeof(i2c_send_t) + 1];
