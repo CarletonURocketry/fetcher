3275fac7baba7f2ade717b6fa71889421c1a732d
Matteo Golin <matteo.golin@gmail.com>
2024-05-12 17:20:02 -0400

    Silence missing prototypes warning.

--- common.mk
+++ common.mk
@@ -21,7 +21,7 @@ OPTIMIZATION = -O2
 # (see https://gcc.gnu.org/onlinedocs/gcc-6.3.0/gcc/Warning-Options.html)
 WARNINGS += -Wall -Wextra -Wshadow -Wundef -Wformat=2 -Wtrampolines -Wfloat-equal
 WARNINGS += -Wbad-function-cast -Wstrict-prototypes -Wpacked
-WARNINGS += -Wno-aggressive-loop-optimizations -Wmissing-prototypes -Winit-self
+WARNINGS += -Wno-aggressive-loop-optimizations -Winit-self -Wmissing-declarations
 WARNINGS += -Wmissing-format-attribute -Wunreachable-code
 WARNINGS += -Wshift-overflow=2 -Wduplicated-cond -Wpointer-arith -Wwrite-strings
 WARNINGS += -Wnested-externs -Wcast-align -Wredundant-decls
